6a1289d066ecc95a5729cf22c3fe5d22e77e6d9d
Russ Painter <russp@geekymonkey.com>
2014-09-17 21:51:28 +0100

    Game manager creates swarm

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : ScratchySprite {
+    public GameObject[] AlienTypes;
+    public int Columns = 10;
+
+    public void CreateSwarm()
+    {
+        int row;
+        int column;
+
+        for (row = 0; row < AlienTypes.Length; row++)
+        {
+            for (column = 0; column < Columns; column++)
+            {
+                var pos = new Vector3(column * 20, row * 20);
+                var alien = Clone(AlienTypes[row], pos);
+            }
+        }
+    }
+
+    public override void OnStart()
+    {
+        CreateSwarm();
+    }
+}
