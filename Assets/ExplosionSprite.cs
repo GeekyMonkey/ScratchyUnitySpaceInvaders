c30b9f6a2804c4eb648e4030aeeaf8dd6091c7eb
Russ Painter <russp@geekymonkey.com>
2014-09-17 21:24:49 +0100

    Explosion prefabs

---
+++ Assets/ExplosionSprite.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionSprite : ScratchySprite
+{
+    public override void OnStart()
+    {
+        Repeat(4, 0.1f, NextCostume);
+        Wait(0.4f, Destroy);
+    }
+}
