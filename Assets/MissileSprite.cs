1721593482342e953f2299102047d591803a530f
Russ Painter <russp@geekymonkey.com>
2014-09-17 19:26:47 +0100

    Missile prefabs

---
+++ Assets/MissileSprite.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileSprite : ScratchySprite {
+
+    public float Speed = 200;
+
+    public override void OnStart()
+    {
+        Wait(5, Destroy);
+    }
+
+    public override void OnUpdate()
+    {
+        Y = Y + Speed * Time.deltaTime;
+    }
+}
