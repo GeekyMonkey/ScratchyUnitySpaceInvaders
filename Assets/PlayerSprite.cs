6f446d24d1dc9cceee57997d78696cd14c07d9ee
Russ Painter <russp@geekymonkey.com>
2014-09-17 18:50:14 +0100

    Player move script

---
+++ Assets/PlayerSprite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSprite : ScratchySprite
+{
+    public float Speed = 100;
+
+    public override void OnUpdate()
+    {
+        float direction = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1;
+        }
+        X += direction * Speed * Time.deltaTime;
+    }
+}
